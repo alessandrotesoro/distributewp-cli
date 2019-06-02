6bf7c1e5ff7fe2f5de432cbd3532f037ace79762
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-06-02 17:04:27 +0100

    checkout repository

--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -28,6 +28,9 @@ var questions = [
 		message: "Enter new version number:",
 		validate: function validate(val) {
 			return val !== '';
+		},
+		when: function (answers) {
+			return answers.confirm;
 		}
 	},
 ];
@@ -42,12 +45,73 @@ class DeployCommand extends Command {
 
 		if ( utilities.fileExists(configFile) ) {
 
+			var data = jsonfile.readFileSync(configFile)
+
 			const results = await inquirer.prompt(allQuestions).then(answers => {
+
+				if (answers.confirm === true) {
+
+					const username = answers.username
+					const password = answers.password
+					const pluginSlug = data.plugin_slug
+					const svnUrl = `https://plugins.svn.wordpress.org/${pluginSlug}`
+					const tempSVNFolder = `${pluginDirectory}/tempsvn`
+
+					const tasks = new Listr([
+						{
+							title: 'Checking out .org repository...',
+							task: ( ctx, task ) => {
+
+								return new Observable(observer => {
+
+									svnUltimate.commands.checkout( svnUrl, tempSVNFolder, {
+										trustServerCert: true,
+										depth: "immediates",
+									}, function( err ) {
+										if ( ! err ) {
+											svnUltimate.commands.update( tempSVNFolder,
+											{
+												username: username,
+												password: password,
+												params: [ '--set-depth infinity trunk' ],
+											},
+											function( err ) {
+												if ( ! err ) {
+													task.title = 'Successfully checked out .org repository'
+													observer.complete();
+												} else {
+													throw new Error( 'Something went wrong while checking out the .org repository.' );
+												}
+											} );
+										}
+									} );
+
+								});
+
+							}
+						},
+					]);
+
+					tasks.run().then( () => {
+
+						this.log( ' ' )
+						this.log( logSymbols.success, `New version successfully deployed.` )
+						this.log( ' ' )
+
+					}).catch( err => {
+						this.error( err )
+					} );
+
+				} else {
+					this.error( 'Deployment aborted.' )
+				}
 
 			});
 
 		} else {
+
 			this.error( 'No config file has been found in this folder. Please run distribute-wp init to configure this folder for deployment.')
+
 		}
 
 	}
